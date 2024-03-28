eeb54734cacf582177abc88028ee2837ca2a2024
Fenil05 <fenilgajjar2265@gmail.com>
2024-03-28 12:39:35 +0530

    create and initialize all routes and its controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
+import commentRoutes from "./routes/comments.js"
+import videoRoutes from "./routes/videos.js"
+import authRoutes from "./routes/auth.js"
 
 const app = express()
 dotenv.config()
@@ -13,6 +17,11 @@ const connect = async()=>{
         console.log(err);
     }
 }
+
+app.use("/api/users",userRoutes)
+app.use("/api/videos",videoRoutes)
+app.use("/api/comments",commentRoutes)
+app.use("/api/auth",authRoutes)
 
 app.listen(8000,()=>{
     connect()
